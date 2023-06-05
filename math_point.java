38ff3a015e529ca45493a34a77e430bf4dd8eebb
hambleply <125007362+scratchfree@users.noreply.github.com>
2023-06-05 13:10:24 +0300

    Update math_point.java

--- math_point.java
+++ math_point.java
@@ -1,15 +1,14 @@
 package project;
 public class math_point {
+    
     String[] the_list_numbers = {"0", "I", "II","III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    String l = the_list_numbers.length;
      int convert_number_correct(String number) throws Exception {
-
-         for(int i = 0; i < the_list_numbers.length; i ++){
-
-             if(number.equals(the_list_numbers[i])){
+         for(int i = 0; i < l; i ++){
+             if(number.equals(l[i])){
                  return i;
              }
          }
          return -1;
      }
-
 }
